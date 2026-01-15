8c1f6688ab811def01f70fd806361410b13ad206
Sherry <yun11261016@gmail.com>
2026-01-14 20:04:11 -0700

    change database

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,16 +1,30 @@
-import { createPool } from "mysql2/promise";
+// import { createPool } from "mysql2/promise";
+import { Pool } from "pg";
 
-const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 60000,
-  dateStrings: true,
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl:
+    connectionString && connectionString.includes("supabase.co")
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
+
+// export default pool;
+// const pool = createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   connectTimeout: 60000,
+//   dateStrings: true,
+// });
+
+// export default pool;
